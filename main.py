89de59aab8994b01c7d70c6c28038d5628ffcf64
DenDen047 <sh.mn.nat@gmail.com>
2022-12-28 15:37:08 +0200

    convert avif to webp

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import sys
 import glob
 import shutil
+from PIL import Image
+import pillow_avif
 from tqdm import tqdm
 from pprint import pprint
 
@@ -17,6 +19,16 @@ def main():
 
     for dir_path in tqdm(dir_paths):
         dir_path = os.path.abspath(dir_path.strip())
+
+        # convert images to webp format if the folder having AVIF images
+        image_fpaths = glob.glob(os.path.join(dir_path, '*.avif'))
+        if len(image_fpaths) > 0:
+            for image_fpath in image_fpaths:
+                with Image.open(image_fpath) as img:
+                    img.save(image_fpath[:-4] + 'webp', format='WebP')
+                os.remove(image_fpath)
+
+        # make a zip
         shutil.make_archive(dir_path, format='zip', root_dir=dir_path)
 
 
