519bb89ad6af60081426f29b74cf3dd75fa56e54
DenDen047 <sh.mn.nat@gmail.com>
2024-08-31 10:00:52 +0200

    bugfix: fix the error, "ZIP does not support timestamps before 1980"

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ import sys
 import glob
 import argparse
 import shutil
+import time
 from PIL import Image
 from tqdm import tqdm
 from pprint import pprint
+import pillow_avif
 
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--file_path', type=str, default='./list.txt')
 argparser.add_argument('--crop_area', type=str, default=None, help='crop area in format of x1,y1,x2,y2')
 args = argparser.parse_args()
+
+
+def update_file_timestamps(directory):
+    min_timestamp = time.mktime((1980, 1, 1, 0, 0, 0, 0, 0, 0))
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_time = os.path.getmtime(file_path)
+            if file_time < min_timestamp:
+                os.utime(file_path, (min_timestamp, min_timestamp))
 
 
 # === MAIN ===
@@ -24,7 +36,7 @@ def main():
         dir_path = os.path.abspath(dir_path.strip())
 
         # convert images to webp format if the folder having AVIF images
-        avif_fpaths = glob.glob(os.path.join(dir_path, '*.avif'))
+        avif_fpaths = sorted(glob.glob(os.path.join(dir_path, '*.avif')))
         if len(avif_fpaths) > 0:
             for image_fpath in avif_fpaths:
                 with Image.open(image_fpath) as img:
@@ -47,7 +59,15 @@ def main():
                     img.crop(crop_area).save(image_fpath)
 
         # make a zip
-        shutil.make_archive(dir_path, format='zip', root_dir=dir_path)
+        try:
+            shutil.make_archive(dir_path, format='zip', root_dir=dir_path)
+        except ValueError as e:
+            if "ZIP does not support timestamps before 1980" in str(e):
+                print(f"Warning: {dir_path} contains files with timestamps before 1980. Updating timestamps...")
+                update_file_timestamps(dir_path)
+                shutil.make_archive(dir_path, format='zip', root_dir=dir_path)
+            else:
+                raise e
 
 
 if __name__ == "__main__":
